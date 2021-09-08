bdd3b9648aa1c7565c4f89074800f54c4a8996a7
lisahuele <lisahuele@gmail.com>
2021-09-08 12:20:57 -0400

    user controller completed

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -27,8 +27,43 @@ const userController = {
         .select('-__v')
         .then(dbUserData => {
             if(!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id!' })
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
             }
+            res.json(dbUserData);
         })
+        .catch(err => res.status(400).json(err))
+    },
+    //create a user
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err))
+    },
+    //update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err))
+    },
+    //delete a user
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(404).json(err))
     }
-}
+};
+
+module.exports = userController;
